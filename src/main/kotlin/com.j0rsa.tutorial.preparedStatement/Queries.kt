2ee4349f5db24f87e22cf61f0e705851d8793daf
keyridan <keyridan@gmail.com>
2020-02-05 17:35:48 +0100

    added reflect for extractValues added reified for getValue

--- src/main/kotlin/com.j0rsa.tutorial.preparedStatement/Queries.kt
+++ src/main/kotlin/com.j0rsa.tutorial.preparedStatement/Queries.kt
@@ -1,17 +1,22 @@
 package com.j0rsa.tutorial.preparedStatement
 
 import com.j0rsa.tutorial.preparedStatement.TransactionManager.currentTransaction
+import org.joda.time.DateTime
+import java.math.BigDecimal
 import java.sql.ResultSet
 import java.util.ArrayList
+import kotlin.reflect.KClass
 
 fun String.exec(): ResultSet? =
     with(currentTransaction().connection.prepareStatement(this)) {
         this.executeQuery()
     }
 
-fun ResultSet?.getValue(name: String): ArrayList<String> = this.map { it.toValue(name) }
+inline fun <reified T : Any> ResultSet?.getValue(name: String): ArrayList<T> =
+    this.map { it.toValue(T::class, name) }
 
-fun ResultSet.toValue(name: String): String = this.getString(name)
+@Suppress("UNCHECKED_CAST")
+fun <T : Any> ResultSet.toValue(kClass: KClass<T>, name: String): T = this.extractValue(kClass, name) as T
 
 fun <T> ResultSet?.map(transform: (ResultSet) -> T): ArrayList<T> {
     val result = arrayListOf<T>()
@@ -21,4 +26,18 @@ fun <T> ResultSet?.map(transform: (ResultSet) -> T): ArrayList<T> {
         }
     }
     return result
+}
+
+@Suppress("IMPLICIT_CAST_TO_ANY")
+private fun <T : Any> ResultSet.extractValue(property: KClass<T>, name: String) = when (property) {
+    DateTime::class -> DateTime(getTimestamp(name))
+    BigDecimal::class -> getBigDecimal(name)
+    Boolean::class -> getBoolean(name)
+    Double::class -> getDouble(name)
+    Float::class -> getFloat(name)
+    Int::class -> getInt(name)
+    Short::class -> getShort(name)
+    Long::class -> getLong(name)
+    String::class -> getString(name)
+    else -> getObject(name, property.java)
 }
